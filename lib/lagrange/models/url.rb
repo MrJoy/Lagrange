808c0e7ee4fc7b91e22708b195e1ecff761ba67a
Jon Frisby <jfrisby@mrjoy.com>
2013-01-01 09:27:35 -0800

    Adding validation rules for URL...

--- lib/lagrange/models/url.rb
+++ lib/lagrange/models/url.rb
@@ -2,27 +2,36 @@ module Lagrange
   module Models
     class URL < Model
       attribute :uuid,           String
-      attribute :url,            String
+      attribute :raw_url,        String
       attribute :canonical_url,  String
       attribute :cleansed_url,   String
       attribute :title,          String
-      attribute :interface_data, AutoVivifyingOpenStruct, default: proc { AutoVivifyingOpenStruct.new }
+      attribute :interface_data, AutoVivifyingOpenStruct, default: proc { |r, p| AutoVivifyingOpenStruct.new }
 
-      attribute :created_at,     DateTime
-      attribute :updated_at,     DateTime
+      attribute :created_at,     DateTime,                default: proc { |r, p| DateTime.now.utc }
+      attribute :updated_at,     DateTime,                default: proc { |r, p| DateTime.now.utc }
 
-      # def url=(val)
-      #   self.canonical_url = val
-      #   self.cleansed_url = val
-      #   self.uuid = Lagrange::DataTypes::URLs.uuid(self.cleansed_url) unless(self.cleansed_url.blank?)
-      #   @url = val
-      # end
+      validates_presence_of :uuid
+      validates_length_of :uuid, equals: 40
+      validates_format_of :uuid, with: /^[a-f0-9]+$/i
 
-      # attr_reader :canonical_url
-      # def canonical_url=(val); @canonical_url = Lagrange::DataTypes::URLs.canonicalize(val).to_s; end
+      validates_presence_of :raw_url, :canonical_url, :cleansed_url
+      validates_length_of :raw_url, :canonical_url, :cleansed_url, within: 1..1024
 
-      # attr_reader :cleansed_url
-      # def cleansed_url=(val); @cleansed_url = Lagrange::DataTypes::URLs.cleanup(val).to_s; end
+      validates_presence_of :created_at
+      validates_presence_of :updated_at
+
+      def raw_url=(val)
+        self.canonical_url = val
+        self.cleansed_url = val
+        self.uuid = Lagrange::DataTypes::URLs.uuid(self.cleansed_url) unless(self.cleansed_url.blank?)
+        @raw_url = val
+      end
+
+    protected
+
+      def canonical_url=(val); @canonical_url = Lagrange::DataTypes::URLs.canonicalize(val).to_s; end
+      def cleansed_url=(val); @cleansed_url = Lagrange::DataTypes::URLs.cleanup(val).to_s; end
 
       # def same_key(other)
       #   return false if(!other.is_a?(URL))
