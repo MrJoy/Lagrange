7305b5db1f473fca9b624561ccb4249d6432ad3a
Jon Frisby <jfrisby@mrjoy.com>
2013-01-01 08:56:38 -0800

    Namespace models...

--- lib/lagrange/models/url.rb
+++ lib/lagrange/models/url.rb
@@ -1,32 +1,34 @@
 module Lagrange
-  class URL < Model
-    attribute :uuid,           String
-    attribute :url,            String
-    attribute :canonical_url,  String
-    attribute :cleansed_url,   String
-    attribute :title,          String
-    attribute :interface_data, AutoVivifyingOpenStruct, default: proc { AutoVivifyingOpenStruct.new }
+  module Models
+    class URL < Model
+      attribute :uuid,           String
+      attribute :url,            String
+      attribute :canonical_url,  String
+      attribute :cleansed_url,   String
+      attribute :title,          String
+      attribute :interface_data, AutoVivifyingOpenStruct, default: proc { AutoVivifyingOpenStruct.new }
 
-    attribute :created_at,     DateTime
-    attribute :updated_at,     DateTime
+      attribute :created_at,     DateTime
+      attribute :updated_at,     DateTime
 
-    # def url=(val)
-    #   self.canonical_url = val
-    #   self.cleansed_url = val
-    #   self.uuid = Lagrange::DataTypes::URLs.uuid(self.cleansed_url) unless(self.cleansed_url.blank?)
-    #   @url = val
-    # end
+      # def url=(val)
+      #   self.canonical_url = val
+      #   self.cleansed_url = val
+      #   self.uuid = Lagrange::DataTypes::URLs.uuid(self.cleansed_url) unless(self.cleansed_url.blank?)
+      #   @url = val
+      # end
 
-    # attr_reader :canonical_url
-    # def canonical_url=(val); @canonical_url = Lagrange::DataTypes::URLs.canonicalize(val).to_s; end
+      # attr_reader :canonical_url
+      # def canonical_url=(val); @canonical_url = Lagrange::DataTypes::URLs.canonicalize(val).to_s; end
 
-    # attr_reader :cleansed_url
-    # def cleansed_url=(val); @cleansed_url = Lagrange::DataTypes::URLs.cleanup(val).to_s; end
+      # attr_reader :cleansed_url
+      # def cleansed_url=(val); @cleansed_url = Lagrange::DataTypes::URLs.cleanup(val).to_s; end
 
-    # def same_key(other)
-    #   return false if(!other.is_a?(URL))
-    #   return false if(other.uuid != self.uuid)
-    #   return true
-    # end
+      # def same_key(other)
+      #   return false if(!other.is_a?(URL))
+      #   return false if(other.uuid != self.uuid)
+      #   return true
+      # end
+    end
   end
 end
--- spec/models/auto_vivifying_open_struct_spec.rb
+++ spec/models/auto_vivifying_open_struct_spec.rb
@@ -1,10 +1,10 @@
-describe Lagrange::AutoVivifyingOpenStruct do
+describe Lagrange::Models::AutoVivifyingOpenStruct do
   def recursively_check(avos)
     table = avos.instance_variable_get(:'@table')
     table.each do |key, value|
       key.should be_a Symbol
       value.should_not be_a Hash
-      if(value.is_a?(Lagrange::AutoVivifyingOpenStruct))
+      if(value.is_a?(Lagrange::Models::AutoVivifyingOpenStruct))
         recursively_check(value)
       elsif(value.is_a?(Fixnum))
         value.should eq 123
@@ -16,13 +16,13 @@ describe Lagrange::AutoVivifyingOpenStruct do
 
   describe "#new" do
     it "should accept a hash" do
-      s = Lagrange::AutoVivifyingOpenStruct.new(foo: "something", "baz" => 123)
+      s = Lagrange::Models::AutoVivifyingOpenStruct.new(foo: "something", "baz" => 123)
 
       recursively_check(s)
     end
 
     it "should recursively turn a deep hash into auto-vivifying structs" do
-      s = Lagrange::AutoVivifyingOpenStruct.new(foo: { bar: { baz: { blah: 123 }}})
+      s = Lagrange::Models::AutoVivifyingOpenStruct.new(foo: { bar: { baz: { blah: 123 }}})
 
       recursively_check(s)
     end
@@ -30,21 +30,21 @@ describe Lagrange::AutoVivifyingOpenStruct do
 
   describe "#method_missing" do
     it "should auto-vivify arbitrarily deep properties" do
-      s = Lagrange::AutoVivifyingOpenStruct.new
+      s = Lagrange::Models::AutoVivifyingOpenStruct.new
       s.foo.bar.baz.blah = 123
 
       recursively_check(s)
     end
 
     it "should recursively convert hashes that are assigned as values" do
-      s = Lagrange::AutoVivifyingOpenStruct.new
+      s = Lagrange::Models::AutoVivifyingOpenStruct.new
       s.foo = { bar: { baz: { blah: 123 } } }
 
       recursively_check(s)
     end
 
     it "should recursively convert hashes that are assigned as values, even when replacing a value" do
-      s = Lagrange::AutoVivifyingOpenStruct.new
+      s = Lagrange::Models::AutoVivifyingOpenStruct.new
       s.foo = { bar: 123 }
       s.foo.bar = { baz: { blah: 123 } }
 
@@ -54,7 +54,7 @@ describe Lagrange::AutoVivifyingOpenStruct do
 
   describe "#merge" do
     it "should recursively convert the provided hash" do
-      s = Lagrange::AutoVivifyingOpenStruct.new
+      s = Lagrange::Models::AutoVivifyingOpenStruct.new
       s.foo.bar = { baz: { blah: 123 } }
       s.foo.bar.merge(whatever: { meh: 123 })
 
@@ -62,7 +62,7 @@ describe Lagrange::AutoVivifyingOpenStruct do
     end
 
     it "should not care whether keys are symbols or strings" do
-      s = Lagrange::AutoVivifyingOpenStruct.new
+      s = Lagrange::Models::AutoVivifyingOpenStruct.new
       s.foo.bar = { baz: { blah: 123 } }
       s.foo.bar.merge("whatever" => { "meh" => 123 }, "baz" => "something")
 
@@ -72,7 +72,7 @@ describe Lagrange::AutoVivifyingOpenStruct do
 
   describe "#as_json" do
     it "should convert the entire hierarchy into a simple hash" do
-      s = Lagrange::AutoVivifyingOpenStruct.new(foo: { bar: { baz: { blah: 123 }}}).as_json
+      s = Lagrange::Models::AutoVivifyingOpenStruct.new(foo: { bar: { baz: { blah: 123 }}}).as_json
 
       s.should be_a(Hash)
       s.keys.should == [:foo]
