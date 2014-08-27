b631a81ef0ebb1fa68d84746aec9dc1e252ab1da
Jon Frisby <jfrisby@mrjoy.com>
2014-08-27 15:18:42 -0700

    Enable safari stuff...

--- snapshot.sh
+++ snapshot.sh
@@ -2,5 +2,5 @@
 
 bin/pinboard/import.rb --user="$PINBOARD_USER" --password="$PINBOARD_PASSWORD"
 bin/delicious/import.rb --user="$DELICIOUS_USER" --password="$DELICIOUS_PASSWORD"
-#bin/safari/pull.rb --go
-#bin/safari/import.rb --go
+bin/safari/pull.rb --go
+bin/safari/import.rb --go
